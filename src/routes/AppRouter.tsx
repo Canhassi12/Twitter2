bf6533ab6c5920a7d70396182a2701e0d43576ec
Canhassi <arthurcanhassi@gmail.com>
2022-11-11 13:50:46 -0300

    feat: Routes of application

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,21 @@
+import {Login, Register, App} from "../pages"
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from "react-router-dom";
+  
+export default function() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/"  element={<Login />}/> 
+
+          <Route path="/register" element={<Register />}/>
+
+          <Route path="/app" element={<App/>}/>   
+        </Routes>
+    </Router>
+  );
+}
